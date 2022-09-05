ec6dbde598834fe02dcf3eeac2c73325452cd8da
lolJona <JonathanDotson@gmail.com>
2022-09-05 09:19:04 -0700

    Update DiceRoller.py

--- DiceRoller.py
+++ DiceRoller.py
@@ -2,6 +2,21 @@
 # Still need to implement exception handling
 # Still need to implement a loop for the moreinfo variables
 import random
+from statistics import mean
+
+# Allows for multiple modes
+def mode(ls):
+    # dictionary to keep count of each value
+    counts = {}
+    # iterate through the list
+    for item in ls:
+        if item in counts:
+            counts[item] += 1
+        else:
+            counts[item] = 1
+    # get the keys with the max counts
+    return [key for key in counts.keys() if counts[key] == max(counts.values())]
+
 
 berapaDice = int(input("How many dice would you like to roll? "))
 if berapaDice <= 0:
@@ -12,11 +27,18 @@ if sided <= 0:
 dicelist = list(range(berapaDice))
 dicelist[0] = 1                                    #Useful if dice values are set to 1 and then multiplied, I guess
 results = (random.choices(range(1,sided), k = berapaDice))
-print("Your dice values are: " + str(results))
+if berapaDice <= 100:
+    print("Your dice values are: " + str(results))
+else:
+    print("Since you wanted to roll more than 100 dice, I'm not gonna print them.")
+average = mean(results)
+moded = mode(results)
+modeamount = results.count(moded[0])
 #print(results)
+stats = print("The average value of your rolls was " + str(average) +
+              ", and the number(s) which showed up most frequently was/were " + str(moded) +
+              ", which appeared " + str(modeamount) + " times!")
+#print(moded)
 requestedno = int(input("Which number do you want info on? "))
 moreinfo = results.count(requestedno)
 print("The number " + str(requestedno) + " showed up " + str(moreinfo) + " times.")
-requestedno2 = int(input("Which other number do you want info on? "))
-moreinfo2 = results.count(requestedno2)
-print("The number " + str(requestedno2) + " showed up " + str(moreinfo2) + " times.")
